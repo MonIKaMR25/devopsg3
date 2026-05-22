cea919b25b0d0daf64b0ae45483391b6774ee05d
Monica Munguia <monika_admi@hotmail.com>
2026-05-21 23:43:49 -0600

    carpeta ejercicios

---
+++ clase5/ejercicios/calculadora.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Función sumar
+sumar_funct(){
+	echo "sumar"
+}
+
+#Función restar
+restar_funct() {
+	echo "restar"
+}
+
+#Función nuliplicación
+multiplicar_funct() {
+	echo "multiplicar"
+}
+
+#Función dividir
+dividir_funct() {
+	echo "dividir"
+}
+
+echo "Inicia operaciones"
+sumar_funct
+restar_funct
+multiplicar_funct
+dividir_funct
+
+
--- clase5/ejercicios/logger.sh
+++ clase5/ejercicios/logger.sh
@@ -1,25 +1,42 @@
 #!/bin/bash
 
+### variables globales
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+BLUE="\033[0;34m"
+YELLOW="\033[0;33m"
+NC="\033[0m"
+
+
+# ES IMPORTANE LIMPIA UN COLOR ANTES DE USARLO
+
+
 ### info, error, ok
 
 ### funciónde log error
 
 log_error() {
-	echo "[ERROR] $1"
+	echo -e "${RED}[ERROR]${NC} $1"
 }
 
 ### función de log info
 
 log_info() {
-	echo "[INFO] $1"
+	echo -e "${GREEN}[INFO]${MC} $1"
 }
 
 ### función de log ok
 
 log_ok() {
-	echo "[OK] $1"
+	echo -e "${BLUE}[OK]${NC} $1"
+}
+
+### función de warning
+log_warning(){
+	echo -e "${YELLOW}[WARNING]${NC} $1"
 }
 
 log_info "Iniciando instalación de apps"
 log_ok "Configuración finalizada"
 log_error "No se puede copiar la aplicación"
+log_warning "exiten algunos errores, favor de revisar"
