530b7e19e72ea12a0f2c203e4e1344fb6f528376
Monica Munguia <monika_admi@hotmail.com>
2026-05-31 01:54:37 -0600

    cofir

--- clase5/ejercicios/poke/deploy-pokemon.sh
+++ clase5/ejercicios/poke/deploy-pokemon.sh
@@ -6,28 +6,51 @@ APP_NAME="pokemonMX"
 APP_DIR="/var/www/html/${APP_NAME}" # RUTA DONDE SE GUARDARÁ LA APLICACIÓN EN EL SERVIDOR
 NGINX_DIR="/etc/nginx/sites-available/${APP_NAME}" # INDICA QUE EN LA RUTA CONTENDRÁ LA CONFIGURACIÓN DE NGINX. SE LLENARÁ A MEDIDA QUE ESTEMOS DESARROLLANDO EL SCRIPT
 
+##colores
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
+###funciones de logger error
+log_error() {    
+    echo -e "${RED}ERROR: $1${NC}" >&2
+}   
+log_warning() {    
+    echo -e "${YELLOW}ADVERTENCIA: $1${NC}" >&2
+}
+log_success() {    
+    echo -e "${GREEN}ÉXITO: $1${NC}"
+}
+log_info() {    
+    echo -e "${BLUE}INFO: $1${NC}"
+}
+
 #1- PRIMER PASO VALIDAR NGINX
 ## VALIDAR INSTALACIÓN DE NGINX
 if ! command -v nginx &> /dev/null
 then
-    echo "NGINX no está instalado. Por favor, instálalo antes de ejecutar este script."
-    exit 1
-else
-    echo "INSTALANDO NGINX..."
+    log_error "NGINX no está instalado."
+    log_info "INSTALANDO NGINX..."
     sudo apt update
     sudo apt install nginx -y  
     sudo systemctl start nginx
     sudo systemctl enable nginx
-    echo "NGINX INSTALADO Y EN EJECUCIÓN."
+    log_success "NGINX INSTALADO Y EN EJECUCIÓN."
+else
+   log_success "NGINX YA ESTÁ INSTALADON."
 fi
 
 #2-CREAR DIRECTORIO DE LA APLICACIÓN
 
 sudo mkdir -p ${APP_DIR}
+log_success "DIRECTORIO ${APP_DIR} CREADO."
 #sudo chown -R $USER:$USER ${APP_DIR}
 
 #3- COPIAR ARCHIVOS DE LA APLICACIÓN AL DIRECTORIO
 sudo cp -r ./* ${APP_DIR}/
+log_success "ARCHIVOS DE LA APLICACIÓN COPIADOS A ${APP_DIR}."
 #rsync -a ./ "$APP_DIR/"
 
 #3- PERMISOS DE LOS ARCHIVOS EN LA RUTA DE LA APLICACIÓN
@@ -55,11 +78,13 @@ EOL
 #5-ACTIVAR LA CONFIGURACIÓN DE NGINX
 # sudo ln -s ${NGINX_DIR} /etc/nginx/sites-enabled
 
-#6- VALIDAR LA CONFIGURACIÓN DE NGINX
+#5 VALIDAR LA CONFIGURACIÓN DE NGINX
+log_info "Validando la configuración de NGINX..."
 sudo nginx -t
 
-#7 REINICIAR NGINX PARA APLICAR LOS CAMBIOS
+#6REINICIAR NGINX PARA APLICAR LOS CAMBIOS
+log_info "Reiniciando NGINX para aplicar los cambios..."
 sudo systemctl restart nginx
 
-#8- MENSAJE DE ÉXITO
-echo "La aplicación ${APP_NAME} ha sido desplegada exitosamente y está disponible en http://localhost/${APP_NAME}"
+#7 MENSAJE DE ÉXITO
+log_success "La aplicación ${APP_NAME} ha sido desplegada exitosamente y está disponible en http://localhost/${APP_NAME}"
